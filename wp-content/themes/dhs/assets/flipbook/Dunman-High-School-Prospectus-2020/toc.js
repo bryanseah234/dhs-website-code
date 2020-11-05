30c43fc2e59b74290dc56b9974d426c734d43b93
bryanseah234 <bryanseah234@gmail.com>
2020-11-05 19:47:08 +0800

    Add files via upload

---
+++ wp-content/themes/dhs/assets/flipbook/Dunman-High-School-Prospectus-2020/toc.js
@@ -0,0 +1,22 @@
+option_PDF1.outline = [
+      {title: "Principal’s Foreword 校长献词", dest: 4},
+      {title: "School Milestones 里程碑", dest: 6},
+      {title: "School Philosophy 办学理念", dest: 8},
+      {title: "Dunmanian Outcomes 德明人的素养", dest: 9},
+      {title: "SAP School Culture 特选学校文化", dest: 10},
+      {title: "Our Dunmanian Edge 德明人的特质", dest: 12},
+      {title: "Our Dunmanian Stories 德明人的故事", dest: 14},
+      {title: "Our Programmes 德明六年综合课程", dest: 19},
+      {title: "Character Development Programme 品格培育计划", dest: 20},
+      {title: "Bicultural Studies Programme 双文化课程", dest: 22},
+      {title: "Humanities and Aesthetics Programme 人文美学课程", dest: 26},
+      {title: "Knowledge Skills Programme 知识研究课程", dest: 30},
+      {title: "Mathematics and Science Programme 数理课程", dest: 32},
+      {title: "Talent Development Programme 人才培育计划", dest: 37},
+      {title: "Student Leadership Development Programme 学生领袖培育计划", dest: 40},
+      {title: "Careers, Scholarships and Higher Education 职业、奖学金兼 高等教育", dest: 42},
+      {title: "Co-Curricular Programme 课程辅助活动", dest: 44},
+      {title: "CCA Achievements 课程辅助活动奖项与 成就", dest: 48},
+      {title: "Be Part of the Dunmanian Family! 成为我们德明大家庭 的一分子吧!", dest: 50},
+];
+
